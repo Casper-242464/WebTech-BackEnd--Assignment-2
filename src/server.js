dce4c85f10be135edd54e9f01c5bc90393f115b1
KhDanial <182223761+C45-p3R@users.noreply.github.com>
2025-12-28 18:17:20 +0500

    barebones server.js

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
