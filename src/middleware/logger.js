cbf650e537614feb0ba92331d4c231b549b3edda
KhDanial <182223761+C45-p3R@users.noreply.github.com>
2025-12-29 22:41:46 +0500

    logger

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -3,7 +3,13 @@ const path = require('path');
 
 const logger = (req, res, next) => {
     const timestamp = new Date().toISOString();
-    const log = `[${timestamp}], ${req.method}, ${req.url}\n`;
+    const log = `Time: [${timestamp}], Method: ${req.method}, URL: ${req.url}, IP: ${req.ip}\n`;
 
     console.log(log);
-}
+
+    fs.appendFile(path.join(__dirname, '..', 'logs', 'server.log'), log, (err) => {
+        if (err) {console.error('Logging Failed', err);}
+    });
+};
+
+module.exports = logger;
--- src/server.js
+++ src/server.js
@@ -3,17 +3,23 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+// VIEW ENGINE SETUP, STATIC FILES
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-
+// IMPORTS
 const weatherRoutes = require('./routes/weatherRoutes');
+const logger = require('./middleware/logger');
 
+// MIDDLEWARE LOGGER
+app.use(logger);
+
+// ROUTES
 app.use('/', weatherRoutes);
 
-
+// START SERVER
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
