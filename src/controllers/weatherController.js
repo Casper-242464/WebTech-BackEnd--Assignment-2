dea0c2fdcc26d4d096f3ebca189d56f0173c31fe
KhDanial <182223761+C45-p3R@users.noreply.github.com>
2025-12-29 23:00:21 +0500

    controller template

---
+++ src/controllers/weatherController.js
@@ -0,0 +1,25 @@
+//WEATHER
+
+//GET
+exports.returnFavorites = (req, res) => {
+//POST
+};
+exports.addFavorite = (req, res) => {
+
+};
+
+
+//WEATHER/CITY
+
+//GET
+exports.getCityWeather = (req, res) => {
+
+};
+//PUT
+exports.updateFavoriteList= (req, res) => {
+
+};
+//DELETE
+exports.deleteFromFavorites = (req, res) => {
+
+};    
